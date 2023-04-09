4ab29f98ca5088a39a84ad1445a00d6f7cead65d
nacaru-w <ignaciocr92@gmail.com>
2023-04-09 19:51:42 +0200

    Adding updateValues function

--- PEC2_Ej2/Ejer2-2-expense-tracker/services/transaction.service.js
+++ PEC2_Ej2/Ejer2-2-expense-tracker/services/transaction.service.js
@@ -30,11 +30,28 @@ class TransactionService {
 
     // Remove transaction by ID
     removeTransaction(id) {
-        transactions = transactions.filter(transaction => transaction.id !== id);
+        transactions = this.transactions.filter(transaction => transaction.id !== id);
 
         this.updateLocalStorage();
 
         init(); // ?? qué hacer con esto
+    }
+
+    updateValues() {
+        // TODO: pensar si va en el servicio o en la callback 
+        const amounts = this.transactions.map(transaction => transaction.amount);
+
+        const total = amounts.reduce((acc, item) => (acc += item), 0).toFixed(2);
+
+        const income = amounts
+            .filter(item => item > 0)
+            .reduce((acc, item) => (acc += item), 0)
+            .toFixed(2);
+
+        const expense = (
+            amounts.filter(item => item < 0).reduce((acc, item) => (acc += item), 0) *
+            -1
+        ).toFixed(2);
     }
 
 }
