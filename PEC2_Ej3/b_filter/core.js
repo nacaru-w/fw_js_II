1d92e8040f454569c23988e00588b8022d9f74c8
nacaru-w <ignaciocr92@gmail.com>
2023-04-12 01:35:34 +0200

    Finishing filter exercises

--- PEC2_Ej3/b_filter/core.js
+++ PEC2_Ej3/b_filter/core.js
@@ -1,17 +1,27 @@
 function onlyEven(array) {
-  // your code here
+  return array.filter(element => element % 2 == 0)
 }
 
 function onlyOneWord(array) {
-  // your code here
+  return array.filter(element => !element.includes(' '))
 }
 
 function positiveRowsOnly(array) {
-  // your code here
+  return array.filter(element => element.every(element => element >= 0))
 }
 
 function allSameVowels(array) {
-  // your code here
+  function sameVowelsInString(string) {
+    let stringOnlyVowels = string.replace(/[^aeiouàáâãäåæèéêëìíîïðñòóôõöøùúûüýÿ]/gi, "");
+    let firstVowel = stringOnlyVowels[0]
+    for (let vowel of stringOnlyVowels) {
+      if (vowel !== firstVowel) {
+        return false
+      }
+    }
+    return true
+  }
+  return array.filter(sameVowelsInString)
 }
 
 module.exports = {
