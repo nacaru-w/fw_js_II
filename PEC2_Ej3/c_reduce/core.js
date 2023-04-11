41ec5ef5fa7d50706382c1b3238bc7f6177b61e7
nacaru-w <ignaciocr92@gmail.com>
2023-04-12 01:44:11 +0200

    Finished exercises dealing with the reduce method

--- PEC2_Ej3/c_reduce/core.js
+++ PEC2_Ej3/c_reduce/core.js
@@ -1,17 +1,30 @@
 function sum(array) {
-  // your code here
+  return array.reduce((accumulator, currentvalue) => accumulator + currentvalue, 0);
 }
 
 function productAll(array) {
-  // your code here
+  return array.reduce((accumulator, currentvalue) => accumulator * currentvalue.reduce((accumulatorInMatrix, currentvalueInMatrix) => accumulatorInMatrix * currentvalueInMatrix, 1), 1)
 }
 
 function objectify(array) {
-  // your code here
+  function objectBuilder(arrayToTurn) {
+    let obj = {};
+    obj[arrayToTurn[0]] = arrayToTurn[1]
+    return obj
+  }
+
+  return array.reduce((accumulator, currentvalue) => Object.assign(accumulator, objectBuilder(currentvalue)), {})
+
 }
 
 function luckyNumbers(array) {
-  // your code here
+  return array.reduce(function (accumulator, currentValue, index, array) {
+    if (index === array.length - 1) {
+      return accumulator + 'and ' + currentValue;
+    } else {
+      return accumulator + currentValue + ', ';
+    }
+  }, 'Your lucky numbers are: ');
 }
 
 module.exports = {
