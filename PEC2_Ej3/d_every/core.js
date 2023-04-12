9173d77c2b73375719911fd11cec5d05204739fc
nacaru-w <ignaciocr92@gmail.com>
2023-04-12 02:15:41 +0200

    Finishing exercises dealing with the every method

--- PEC2_Ej3/d_every/core.js
+++ PEC2_Ej3/d_every/core.js
@@ -2,14 +2,15 @@
 // are even numbers.
 
 function allEven(input) {
-  return input;
+  return input.every(element => element % 2 == 0);
 }
 
 // Check to see if all elements in an array
 // are of the same type.
 
 function allSameType(input) {
-  return input;
+  let typeOfFirstEl = typeof (input[0])
+  return input.every(element => typeof (element) == typeOfFirstEl);
 }
 
 // Check to see if every element in the matrix is
@@ -17,14 +18,27 @@ function allSameType(input) {
 // greater than 0.
 
 function positiveMatrix(input) {
-  return input;
+  function checkArrayNumbers(array) {
+    return array.every(element => element > 0)
+  }
+  return input.every(element => Array.isArray(element) && checkArrayNumbers(element))
 }
 
 // Check that all items in an array are strings
 // and that they all only contain the same vowels.
 
 function allSameVowels(input) {
-  return input;
+  function sameVowelsInString(string) {
+    let stringOnlyVowels = string.replace(/[^aeiouàáâãäåæèéêëìíîïðñòóôõöøùúûüýÿ]/gi, "");
+    let firstVowel = stringOnlyVowels[0]
+    for (let vowel of stringOnlyVowels) {
+      if (vowel !== firstVowel) {
+        return false
+      }
+    }
+    return true
+  }
+  return input.every(element => typeof (element) == "string" && sameVowelsInString(element));
 }
 
 module.exports = {
