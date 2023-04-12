304a11f74468d83c9d1bf07e2a003b58995c18ab
nacaru-w <ignaciocr92@gmail.com>
2023-04-12 02:43:16 +0200

    Finishing exercises that deal with some

--- PEC2_Ej3/e_some/core.js
+++ PEC2_Ej3/e_some/core.js
@@ -1,29 +1,35 @@
 // Check to see if any of the elements in the
 // array are numbers greater than 10.
 
-function anyGreaterThan10 (input) {
-  return input;
+function anyGreaterThan10(input) {
+  return input.some(element => element > 10);
 };
 
 // Check to see if any of the strings in
 // the array is longer than 10 characters.
 
-function longWord (input) {
-  return input;
+function longWord(input) {
+  return input.some(element => element.length > 10);
 };
 
 // Check to see if any of the elements in
 // the matrix are true.
 
-function truePossibilities (input) {
-  return input;
+function truePossibilities(input) {
+  function checkArrays(array) {
+    return array.some(element => element)
+  }
+  return input.some(element => checkArrays(element));
 };
 
 // Check to see if 'Lost' is in
 // the phrase (using some).
 
-function lostCarcosa (input) {
-  return input;
+function lostCarcosa(input) {
+  function lostInString(string) {
+    return string.toLowerCase().includes("lost")
+  }
+  return input.some(element => lostInString(element));
 };
 
 module.exports = {
