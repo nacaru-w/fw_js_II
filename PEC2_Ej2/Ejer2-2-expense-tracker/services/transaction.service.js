6060a650ea69039544642032390d223b8d1dad47
nacaru-w <ignaciocr92@gmail.com>
2023-04-13 10:45:42 +0200

    Commenting deprecated code

--- PEC2_Ej2/Ejer2-2-expense-tracker/services/transaction.service.js
+++ PEC2_Ej2/Ejer2-2-expense-tracker/services/transaction.service.js
@@ -66,21 +66,21 @@ class TransactionService {
         return expense;
     }
 
-    // Update the balance, income and expense
-    updateValues() {
-        const amounts = this.transactions.map(transaction => transaction.amount);
+    // Update the balance, income and expense (not used in the end)
+    // updateValues() {
+    //     const amounts = this.transactions.map(transaction => transaction.amount);
 
-        const total = amounts.reduce((acc, item) => (acc += item), 0).toFixed(2);
+    //     const total = amounts.reduce((acc, item) => (acc += item), 0).toFixed(2);
 
-        const income = amounts
-            .filter(item => item > 0)
-            .reduce((acc, item) => (acc += item), 0)
-            .toFixed(2);
+    //     const income = amounts
+    //         .filter(item => item > 0)
+    //         .reduce((acc, item) => (acc += item), 0)
+    //         .toFixed(2);
 
-        const expense = (
-            amounts.filter(item => item < 0).reduce((acc, item) => (acc += item), 0) *
-            -1
-        ).toFixed(2);
-    }
+    //     const expense = (
+    //         amounts.filter(item => item < 0).reduce((acc, item) => (acc += item), 0) *
+    //         -1
+    //     ).toFixed(2);
+    // }
 
 }
